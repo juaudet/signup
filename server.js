08aef4d7cfe14a28c385480167a11628f4122637
Mousselmal Akram <Akram-mohamed.mousselmal.1@ens.etsmtl.ca>
2018-06-07 00:16:20 -0400

    Reste du repository angular

--- server.js
+++ server.js
@@ -1,17 +1,5 @@
 const express = require('express');
-const http = require('http')
-const path = require('path');
-
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + 'dist/index.html'));
-});
-
-const port = process.env.PORT || 3000;
-app.set('port', port);
-
-const server = http.createServer(app);
-server.listen(port, () => console.log('running'));
+app.use(express.static(__dirname + '/dist/e18-analytique'));
+app.listen(process.env.PORT || 8080);
